47aec3fa49a46912b51b84f2ca52a34f35b65ea0
rohit kadam <rohitkadamrb@gmail.com>
2017-11-16 00:16:20 +1300

    Create Gig Model

---
+++ Gighub/Models/Gig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gighub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+
+
+    }
+
+
+
+}
