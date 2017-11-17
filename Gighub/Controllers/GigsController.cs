c7588a864e57a0f0ab6bf67c22cf3456b286cfb4
rohit kadam <rohitkadamrb@gmail.com>
2017-11-17 22:11:35 +1300

    Validations

--- Gighub/Controllers/GigsController.cs
+++ Gighub/Controllers/GigsController.cs
@@ -31,11 +31,16 @@ namespace Gighub.Controllers
         [Authorize]
         public ActionResult Create(GigFormViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
 
+                return View("Create", viewModel);
+            }
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
